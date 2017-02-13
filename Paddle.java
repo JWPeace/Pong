8611350b4e9ce192f34a6beced444a8324516208
V01dPr0xy <DBevins@student.neumont.edu>
2017-02-13 09:52:36 -0700

    Paddle, Ball, and the GUI

---
+++ Paddle.java
@@ -0,0 +1,35 @@
+package edu.neumont.csc150.Pong.view;
+
+public class Paddle{
+	
+	private int sizeX, sizeY, locationX, locationY;
+	
+	public int getSizeX(){
+		return sizeX;
+	}	
+	public void setSizeX(int sizeX){
+		this.sizeX = sizeX;
+	}
+	
+	public int getSizeY(){
+		return sizeY;
+	}	
+	public void setSizeY(int sizeY){
+		this.sizeY = sizeY;
+	}
+	
+	public int getLocationX(){
+		return locationX;
+	}	
+	public void setLocationX(int locationX){
+		this.locationX = locationX;
+	}
+	
+	public int getLocationY(){
+		return locationY;
+	}	
+	public void setSizeY(int locationY){
+		this.locationY = locationY;
+	}	
+	
+}
---
+++ PongGUI.java
@@ -0,0 +1,6 @@
+package edu.neumont.csc150.Pong.view;
+
+public class PongGUI{
+	
+	
+}
