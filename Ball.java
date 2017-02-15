4fed428947fd4642a99b42350ef9f78bd60bb684
V01dPr0xy <DBevins@student.neumont.edu>
2017-02-15 16:13:45 -0700

    Hopefully finished Paddle and Ball

--- Ball.java
+++ Ball.java
@@ -1,21 +1,21 @@
 package edu.neumont.csc150.Pong.view;
 
-public class Ball{
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Ball extends JPanel {
 	
-	private int sizeX, sizeY, locationX, locationY;
+	private int frameWidth, frameHeight, diameter = frameWidth / 25, locationX = frameWidth / 2, locationY = frameHeight / 2, xVeloc = 5, yVeloc = 5;
 	
-	public int getSizeX(){
-		return sizeX;
+	public int getDiameter(){
+		return diameter;
 	}	
-	public void setSizeX(int sizeX){
-		this.sizeX = sizeX;
-	}
-	
-	public int getSizeY(){
-		return sizeY;
-	}	
-	public void setSizeY(int sizeY){
-		this.sizeY = sizeY;
+	public void setDiameter(int diameter){
+		this.diameter = diameter;
 	}
 	
 	public int getLocationX(){
@@ -28,7 +28,76 @@ public class Ball{
 	public int getLocationY(){
 		return locationY;
 	}	
+	
 	public void setLocationY(int locationY){
 		this.locationY = locationY;
+	}
+	
+	/**
+	 * @return the frameWidth
+	 */
+	public int getFrameWidth() {
+		return frameWidth;
+	}
+	/**
+	 * @param frameWidth the frameWidth to set
+	 */
+	public void setFrameWidth(int frameWidth) {
+		this.frameWidth = frameWidth;
+	}
+	/**
+	 * @return the frameHeight
+	 */
+	public int getFrameHeight() {
+		return frameHeight;
+	}
+	/**
+	 * @param frameHeight the frameHeight to set
+	 */
+	public void setFrameHeight(int frameHeight) {
+		this.frameHeight = frameHeight;
+	}
+	
+	/**
+	 * @return the xVeloc
+	 */
+	public int getxVeloc() {
+		return xVeloc;
+	}
+	/**
+	 * @param xVeloc the xVeloc to set
+	 */
+	public void setxVeloc(int xVeloc) {
+		this.xVeloc = xVeloc;
+	}
+	/**
+	 * @return the yVeloc
+	 */
+	public int getyVeloc() {
+		return yVeloc;
+	}
+	/**
+	 * @param yVeloc the yVeloc to set
+	 */
+	public void setyVeloc(int yVeloc) {
+		this.yVeloc = yVeloc;
+	}
+	
+	public Rectangle getBounds(){
+		return new Rectangle(locationX, locationY, diameter, diameter);
+	}
+	
+	public void collision(Paddle p){
+		if(this.getBounds().intersects(getBounds(p.getBounds()))){
+			yVeloc *= -1;
+			xVeloc *= -1;
+		}
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(locationX, locationY, diameter, diameter);
 	}
 }
--- Paddle.java
+++ Paddle.java
@@ -1,19 +1,91 @@
 package edu.neumont.csc150.Pong.view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Paddle implements KeyListener{
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Paddle extends JPanel implements KeyListener {
 	
-	private int locationY, width, height, yVeloc;
+	private int frameWidth, frameHeight, locationX, locationY = frameHeight / 2, width = frameWidth / 25, height = frameHeight / 5, yVeloc = 5;
 	private boolean isLeft;
 	
 	public Paddle() {}
 	
-	public Paddle(boolean isLeft, int width, int height) {
+	public Paddle(boolean isLeft, int width, int height, int yVeloc, int frameWidth, int frameHeight) {
 		this.setSide(isLeft);
+		this.setWidth(width);
+		this.setHeight(height);
+		this.setYVeloc(yVeloc);
+		this.setFrameWidth(frameWidth);
+		this.setFrameHeight(frameHeight);
 	}
 	
+	/**
+	 * @return the locationX
+	 */
+	public int getLocationX() {
+		return locationX;
+	}
+
+	/**
+	 * @param locationX the locationX to set
+	 */
+	public void setLocationX(int locationX) {
+		if(isLeft == true){
+			locationX = frameWidth / 25;
+		}else{
+			locationX = frameWidth - 2 * (frameWidth / 25);
+		}
+		this.locationX = locationX;
+	}
+
+	/**
+	 * @return the yVeloc
+	 */
+	public int getyVeloc() {
+		return yVeloc;
+	}
+
+	/**
+	 * @param yVeloc the yVeloc to set
+	 */
+	public void setyVeloc(int yVeloc) {
+		this.yVeloc = yVeloc;
+	}
+
+	/**
+	 * @return the frameWidth
+	 */
+	public int getFrameWidth() {
+		return frameWidth;
+	}
+
+	/**
+	 * @param frameWidth the frameWidth to set
+	 */
+	public void setFrameWidth(int frameWidth) {
+		this.frameWidth = frameWidth;
+	}
+
+	/**
+	 * @return the frameHeight
+	 */
+	public int getFrameHeight() {
+		return frameHeight;
+	}
+
+	/**
+	 * @param frameHeight the frameHeight to set
+	 */
+	public void setFrameHeight(int frameHeight) {
+		this.frameHeight = frameHeight;
+	}
+
 	public boolean isLeft(){
 		return this.isLeft();
 	}
@@ -46,31 +118,38 @@ public class Paddle implements KeyListener{
 		this.locationY = locationY;
 	}
 	
-	public int setLocationX(int width, boolean isLeft){
-		if(isLeft == true){
-			return width / 25;
-		}else{
-			return width - 2 * (width / 25);
-		}
+	public int getYVeloc(){
+		return this.yVeloc;
 	}
 	
-	public void shape(int frameWdith){
-		
+	public void setYVeloc(int yVeloc){
+		this.yVeloc = yVeloc;
+	}
+	
+	public Rectangle getBounds(){
+		return new Rectangle(locationX, locationY, width, height);
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(locationX, locationY, width, height);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent ke) {
 		if(isLeft == true){
 			if(ke.getKeyLocation() == KeyEvent.VK_W){
-				
+				this.locationY += yVeloc;
 			}else if(ke.getKeyLocation() == KeyEvent.VK_S){
-				
-			}
+				this.locationY -= yVeloc;
+ 			}
 		}else if(isLeft == false){
 			if(ke.getKeyLocation() == KeyEvent.VK_UP){
-				
+				this.locationY += yVeloc;
 			}else if(ke.getKeyLocation() == KeyEvent.VK_DOWN){
-				
+				this.locationY -= yVeloc;
 			}
 		}		
 	}
